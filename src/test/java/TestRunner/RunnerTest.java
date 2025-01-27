2aed5d49ed9e07219cb11c55bab6de2802998d44
paresh shinde <sbshinde94@gmail.com>
2025-01-27 16:39:52 +0530

    TestRunner class is created

--- src/test/java/TestRunner/RunnerTest.java
+++ src/test/java/TestRunner/RunnerTest.java
@@ -1,5 +1,17 @@
 package TestRunner;
 
-public class RunnerTest {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+features= {"src/test/java/Featurefiles"},
+glue= {"StepDefinition"},
+dryRun=true,
+monochrome=true
+
+		
+		)
+
+public class RunnerTest extends AbstractTestNGCucumberTests {
 
 }
